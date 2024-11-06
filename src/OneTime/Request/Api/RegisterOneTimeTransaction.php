88b7a50e7607341325c1347585957136d8851ee4
Dominik Płachta <dplachta03@gmail.com>
2024-11-06 17:34:21 +0100

    Add OneTime Przelewy24 gateway

---
+++ src/OneTime/Request/Api/RegisterOneTimeTransaction.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusPrzelewy24Plugin\OneTime\Request\Api;
+
+use Payum\Core\Request\Generic;
+
+class RegisterOneTimeTransaction extends Generic
+{
+}
