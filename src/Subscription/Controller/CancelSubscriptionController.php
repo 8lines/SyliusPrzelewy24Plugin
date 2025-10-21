b6ecaad77d2f9dd583555aae2cd3715053cd6124
Dominik Płachta <dplachta03@gmail.com>
2025-10-21 13:33:43 +0200

    Add cancel subscription endpoint

---
+++ src/Subscription/Controller/CancelSubscriptionController.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusPrzelewy24Plugin\Subscription\Controller;
+
+use BitBag\SyliusPrzelewy24Plugin\Subscription\Entity\SyliusCustomerAsSubscriberInterface;
+use BitBag\SyliusPrzelewy24Plugin\Subscription\Repository\SubscriptionRepositoryInterface;
+use BitBag\SyliusPrzelewy24Plugin\Subscription\Transitions\SubscriptionIntervalTransitions;
+use BitBag\SyliusPrzelewy24Plugin\Subscription\Transitions\SubscriptionTransitions;
+use Sylius\Abstraction\StateMachine\StateMachineInterface;
+use Sylius\Bundle\ApiBundle\Context\UserContextInterface;
+use Sylius\Component\Core\Model\ShopUserInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Webmozart\Assert\Assert;
+
+final readonly class CancelSubscriptionController
+{
+    public function __construct(
+        private UserContextInterface $userContext,
+        private SubscriptionRepositoryInterface $subscriptionRepository,
+        private StateMachineInterface $stateMachine,
+    ) {
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        /** @var ShopUserInterface|null $shopUser */
+        $shopUser = $this->userContext->getUser();
+
+        /** @var SyliusCustomerAsSubscriberInterface $customer */
+        $customer = $shopUser?->getCustomer();
+
+        Assert::notNull(
+            value: $customer,
+            message: 'Customer not found.',
+        );
+
+        $subscription = $this->subscriptionRepository->findOneBy([
+            'id' => $request->get('id'),
+            'owner' => $customer->getPrzelewy24Subscriber(),
+        ]);
+
+        Assert::notNull(
+            value: $subscription,
+            message: 'Subscription not found.',
+        );
+
+        foreach ($subscription->getSchedule()->getIntervals() as $interval) {
+            if (false === $interval->isScheduled() && false === $interval->isFulfilled()) {
+                continue;
+            }
+
+            $this->stateMachine->apply(
+                subject: $interval,
+                graphName: SubscriptionIntervalTransitions::GRAPH,
+                transition: SubscriptionIntervalTransitions::TRANSITION_ABORT,
+            );
+        }
+
+        $subscription->getConfiguration()->setCard(null);
+
+        if (false === $subscription->isAborted()) {
+            $this->stateMachine->apply(
+                subject: $subscription,
+                graphName: SubscriptionTransitions::GRAPH,
+                transition: SubscriptionTransitions::TRANSITION_ABORT,
+            );
+        }
+
+        $this->subscriptionRepository->add($subscription);
+
+        return new JsonResponse(status: Response::HTTP_NO_CONTENT);
+    }
+}
